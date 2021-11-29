a58db05eb709a41abee6a8941b1baac45b703508
DUOLabs333 <dvdugo333@gmail.com>
2021-11-29 12:01:29 -0500

    Stream responses if larger than 10M -- should solve problems with downloads that can also be webpages. No webapge should be more than 10M, right?

--- proxy/history.py
+++ proxy/history.py
@@ -5,10 +5,13 @@ from bs4 import BeautifulSoup
 def responseheaders(flow):
     if not flow.request.headers['accept'].startswith('text/html,application/xhtml+xml,application/xml'):
         flow.response.stream = True
-    
+
 def response(flow: http.HTTPFlow):
     if flow.request.headers['accept'].startswith('text/html,application/xhtml+xml,application/xml'):
-        soup=BeautifulSoup(flow.response.content)
+        try:
+            title=BeautifulSoup(flow.response.content).title.string
+        except:
+            return
         file=str(datetime.fromtimestamp(int(flow.response.timestamp_start))).split(' ')[0]
         with open('../logs/ '[:-1]+file,'a+') as f:
-            f.write(str(datetime.fromtimestamp(int(flow.response.timestamp_start)))+ ' '+soup.title.string+' '+str(flow.request.url)+'\n\n')
+            f.write(str(datetime.fromtimestamp(int(flow.response.timestamp_start)))+ ' '+title+' '+str(flow.request.url)+'\n\n')
